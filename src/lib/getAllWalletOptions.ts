7297203d638ec4ed0ebfe63df98e941b9625e96e
Tommy Johnson <tommyjohnson1028@gmail.com>
2021-12-03 17:33:18 -0800

    load and deserialize the TokenAccounts

--- src/lib/getAllWalletOptions.ts
+++ src/lib/getAllWalletOptions.ts
@@ -1,8 +1,14 @@
 import { Program } from '@project-serum/anchor';
 import { getAllOptionAccounts, PsyAmericanIdl } from "@mithraic-labs/psy-american";
+import { AccountInfo, AccountLayout, TOKEN_PROGRAM_ID } from '@solana/spl-token';
 
 export const getAllWalletOptions = async (program: Program) => {
   // Load all the PsyOptions option markets
   const optionMarkets = await getAllOptionAccounts(program);
   console.log('*** optionMarkets', optionMarkets);
+  // Load all SPL Mint addresses of the connected wallet
+  const resp = await program.provider.connection.getTokenAccountsByOwner(program.provider.wallet.publicKey, {programId: TOKEN_PROGRAM_ID});
+  const tokenAccounts = resp.value.map(({account}) => AccountLayout.decode(account.data) as AccountInfo);
+  console.log('*** token accounts = ', tokenAccounts);
+  // TODO: Filter option markets by optionMint if the user's tokens include the optionMint
 };
