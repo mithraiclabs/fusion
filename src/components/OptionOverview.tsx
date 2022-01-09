326e10834b4bee53c13e7799d8dcd969e54fc4f8
ClareSegrue <81589284+ClareSegrue@users.noreply.github.com>
2022-01-08 21:09:43 -0500

    Separating Option Market Utils into individual Values

--- src/components/OptionOverview.tsx
+++ src/components/OptionOverview.tsx
@@ -1,29 +1,63 @@
-import { Box, Card, CardContent} from "@material-ui/core";
+import { Box, Card, CardContent } from "@material-ui/core";
 import React from "react";
-import { displayHeader } from "../lib/optionMarketUtils";
+import { displayExpiration, displayHeader, displayProjectSymbol, displayQuoteToken, displayStrikePrice, displayUnderlyingAmt } from "../lib/optionMarketUtils";
 import { MintInfoWithKey, OptionAccounts, Project } from "../types";
 import styles from "../styles/OptionOverview.module.scss";
 
+/*
 
 
-/** @jsxImportSource theme-ui */
+        {displayHeader(
+          project,
+          optionAccounts.optionMarket,
+          mintInfos[optionAccounts.optionMarket.underlyingAssetMint.toString()],
+          mintInfos[optionAccounts.optionMarket.quoteAssetMint.toString()]
+        )}
+
+
+
+*/
+
 
 const OptionOverview: React.FC<{
   project: Project;
+
   optionAccounts: OptionAccounts;
   mintInfos: Record<string, MintInfoWithKey>;
 }> = ({ optionAccounts, project, mintInfos }) => {
   return (
-    <Card className = {styles["card"]} variant="outlined">
-      <CardContent className = "card-content">
-        {displayHeader(
-          project,
-          optionAccounts.optionMarket, 
-          mintInfos[optionAccounts.optionMarket.underlyingAssetMint.toString()],
-          mintInfos[optionAccounts.optionMarket.quoteAssetMint.toString()]
+    <Card className={styles["card"]} variant="outlined">
+      <CardContent className="card-content">
+        <p>Underlying Amount: {displayUnderlyingAmt(
+          optionAccounts.optionMarket,
+          mintInfos[optionAccounts.optionMarket.underlyingAssetMint.toString()]
         )}
+        </p>
+
+        <p>
+          Project Symbol: {displayProjectSymbol(project)}
+        </p>
+
+        <p>
+          Strike Price: {displayStrikePrice(optionAccounts.optionMarket,
+          mintInfos[optionAccounts.optionMarket.underlyingAssetMint.toString()],
+          mintInfos[optionAccounts.optionMarket.quoteAssetMint.toString()])}
+        </p>
+
+        <p>
+          Underlying Mint: {displayQuoteToken(mintInfos[optionAccounts.optionMarket.underlyingAssetMint.toString()])}
+          </p>
+        
+        <p>
+          Quote Mint: {displayQuoteToken(mintInfos[optionAccounts.optionMarket.quoteAssetMint.toString()])}
+        </p>
+
+        <p>
+          Expiration: {displayExpiration(optionAccounts.optionMarket)}
+        </p>
+
       </CardContent>
-    </Card> 
+    </Card>
 
 
   );
