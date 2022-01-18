604ea0edce689a04c847651839e57bd1b6a87287
ClareSegrue <81589284+ClareSegrue@users.noreply.github.com>
2022-01-17 21:53:09 -0500

    Removing old slider checkbox, table styling

--- src/components/TreasuryFormNew.tsx
+++ src/components/TreasuryFormNew.tsx
@@ -2,7 +2,7 @@ import { Switch } from "@material-ui/core";
 import styles_forms from "../styles/Forms.module.scss";
 import styles_treasury from "../styles/Treasury.module.scss";
 import { DateSelector } from "./DatePicker.module";
-import SliderCheckBox from "./SliderCheckbox";
+
 const TreasuryFormNew = () => (
   <form className={styles_forms.form}>
     <h1>New Token Grant</h1>
@@ -10,86 +10,81 @@ const TreasuryFormNew = () => (
     <fieldset>
       <table>
         <div className="input-group">
-          <tr className="row-one">
+          <tr>
             <th>
               <label htmlFor="mintToken">Mint Token</label>
             </th>
-            <th>
+            <td>
               <Switch />
-            </th>
+            </td>
           </tr>
           <tr>
             <th>
               <label htmlFor="amount">Amount</label>
             </th>
-            <th>
+            <td>
               <input id="amount" type="text" />
-            </th>
+            </td>
           </tr>
           <tr>
             <th>
               <label htmlFor="amount">Issue Options?</label>
             </th>
-            <th>
+            <td>
               <Switch />
-            </th>
+            </td>
           </tr>
           <tr>
             <th>
               <label htmlFor="duration">Duration</label>
             </th>
-            <th>
+            <td>
               <input id="duration" type="text" />
-            </th>
+            </td>
           </tr>
           <tr>
             <th>
               <label htmlFor="initialCliff">Initial Cliff</label>
             </th>
-            <th>
+            <td>
               <input id="initialCliff" type="text" />
-            </th>
+            </td>
           </tr>
           <tr>
             <th>
-              {" "}
               <label htmlFor="vestPeriod">Vest Period</label>
             </th>
-            <th>
+            <td>
               <DateSelector />
-            </th>
+            </td>
           </tr>
           <tr>
             <th>
-              {" "}
               <label htmlFor="amountPerPeriod">Amount per Period</label>
             </th>
-            <th>
+            <td>
               <input id="amountPerPeriod" type="text" />
-            </th>
+            </td>
           </tr>
           <tr>
             <th>
-              {" "}
-              <label htmlFor="numberOfPeriods">Number of Periods</label>
+                <label htmlFor="numberOfPeriods">Number of Periods</label>
             </th>
-            <th>
+            <td>
               <input id="numberOfPeriods" type="text" />
-            </th>
+            </td>
           </tr>
 
           <tr>
             <th>
-              {" "}
               <label htmlFor="recipient">Recipient(s)</label>
             </th>
-            <th>
-              {" "}
+            <td>
               <input id="recipient" type="text" />
               <fieldset className={styles_treasury.experiment}>
                 <button className={styles_treasury.experiment}>+</button>
               </fieldset>
-            </th>
+            </td>
           </tr>
 
   
