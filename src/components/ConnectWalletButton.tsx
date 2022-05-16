adb3a239297ea1fd044950d7479298d21635ea78
Tommy Johnson <tommyjohnson1028@gmail.com>
2022-05-16 11:53:09 -0700

    tweak wallet connect further

--- src/components/ConnectWalletButton.tsx
+++ src/components/ConnectWalletButton.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { WalletMultiButton } from "@solana/wallet-adapter-material-ui";
 import { CSSProperties } from "@material-ui/core/styles/withStyles";
 import { makeStyles } from "@material-ui/core";
+import { useWallet } from "@solana/wallet-adapter-react";
 
 const useStyles = makeStyles((theme) => {
   let baseButton = {};
@@ -17,9 +18,17 @@ export const ConnectWalletButton: React.VFC<{ style?: CSSProperties }> = ({
   style,
 }) => {
   const classes = useStyles();
+  const { publicKey, wallet } = useWallet();
+
+  const base58 = useMemo(() => publicKey?.toBase58(), [publicKey]);
+  const content = useMemo(() => {
+    if (!wallet || !base58) return "Connect Wallet";
+    return base58.slice(0, 4) + ".." + base58.slice(-4);
+  }, [wallet, base58]);
+
   return (
     <WalletMultiButton className={classes.baseButton} style={style}>
-      Connect Wallet
+      {content}
     </WalletMultiButton>
   );
 };
